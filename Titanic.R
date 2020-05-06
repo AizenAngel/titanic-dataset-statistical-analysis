f1e5eb1dab5eb3834c74ba3cb23dc2bc62b12b88
AizenAngel <boza.mitrovic@gmail.com>
2020-05-06 16:32:12 +0200

    Fixing bug when some NA values were not detected

--- Titanic.R
+++ Titanic.R
@@ -1,53 +1,94 @@
 #install.packages("imputeTS")
 library("imputeTS")
 
-read_data <- function (filePath){
-  data <- read.csv(filePath)
-  
-  return (data)
-}
+# mean(train_data$Age, na.rm = T)   = 29.69912
+# median(train_data$Age, na.rm = T) = 28
+# mean(test_data$Age, na.rm = T)    = 30.27259
+# median(test_data$Age, na.rm = T)  = 27
 
-read_test_data <- function(dataPath, classPath){
-  data <- read_data(dataPath)
-  classes <- read_data(classPath)
-  data$Survived <- classes$Survived
+read_data <- function (trainPath, testPath, testClassPath){
+  train_data <- read_train_data(trainPath)
+  test_data <- read_test_data(testPath, testClassPath)
   
-  return (data)
-}
-
-combine_test_train_data <- function (trainPath, testPath, classPath){
-  train_data <- read_data(trainPath)
-  test_data <- read_test_data(testPath, classPath)
   total_data <- rbind(train_data, test_data)
   
   return (total_data)
 }
 
-age_median_replace <- function(dataFrame){
-  age_median <- median(dataFrame$Age, na.rm = T)
-  dataFrame$Age <- na_replace(dataFrame$Age, age_median)
+convert_to_numeric <- function(dataFrame){
+  dataFrame$PassengerId = as.double(dataFrame$PassengerId)
+  dataFrame$Survived = as.double(dataFrame$Survived)
+  dataFrame$Pclass = as.double(dataFrame$Pclass)
+  dataFrame$Age = as.double(dataFrame$Age)
+  dataFrame$SibSp = as.double(dataFrame$SibSp)
+  dataFrame$Fare = as.double(dataFrame$Fare)
+  
+  return (dataFrame)
+}
+
+read_train_data <- function(dataPath){
+  train_data <- read.csv(dataPath, na.strings = "", colClasses = "character")
+  
+  return (convert_to_numeric(train_data))
+}
+
+read_test_data <- function(dataPath, classPath){
+  data <- read.csv(dataPath, na.strings = "", colClasses = "character")
+  classes <- read.csv(classPath, na.strings = "", colClasses = "character")
+  data$Survived <- classes$Survived
+  
+  return (convert_to_numeric(data))
+}
+
+
+deal_with_Embarked_NA <- function(dataFrame){
+  #dataFrame[(is.na(dataFrame$Embarked)),]
+  #unique(dataFrame$Embarked)
+  #print(mean(dataFrame[(which(dataFrame$Embarked == "S")),]$Fare, na.rm = T))
+  #print(mean(dataFrame[(which(dataFrame$Embarked == "C")),]$Fare, na.rm = T))
+  #print(mean(dataFrame[(which(dataFrame$Embarked == "Q")),]$Fare, na.rm = T))
+  
+  #dataFrame$Embarked <- na_replace(dataFrame$Embarked, 'C')
+  dataFrame$Embarked <- ifelse(is.na(dataFrame$Embarked), 
+                               'C', dataFrame$Embarked)
+  
+  return (dataFrame)
+}
+
+deal_with_Cabin_NA <- function(dataFrame) {
+  dataFrame = within(dataFrame, rm("Cabin"))
+
+  return(dataFrame)  
+}
+
+deal_with_Fare_NA <- function(dataFrame) {
+  #print(dataFrame[(which(is.na(dataFrame$Fare))),])
+  m1 <- mean(dataFrame[(which(dataFrame$Embarked == "S" & dataFrame$Sex == "male" & dataFrame$Pclass == 3)),]$Fare, na.rm=T)
+  dataFrame$Fare <- na_replace(dataFrame$Fare, m1)
+  
+  return (dataFrame)
+}
+
+deal_with_NA_values <- function(dataFrame){
+  sapply(dataFrame, function(x) sum(is.na(x)))
+  
+  dataFrame <- deal_with_Cabin_NA(dataFrame)
+  dataFrame <- deal_with_Embarked_NA(dataFrame)
+  dataFrame <- deal_with_Fare_NA(dataFrame)
+  
+  sapply(dataFrame, function(x) sum(is.na(x)))
   
   return (dataFrame)
 }
 
 #####################################################################################
 
-train_data <- read_data("train.csv")
-test_data <- read_data("test.csv")
- 
-# R.summary() == Python.describe()
-summary(train_data)
+main <- function(){
+  
+  dataFrame <- read_data("train.csv", "test.csv", "classes.csv")  
+  dataFrame <- deal_with_NA_values(dataFrame)
+  
+  head(dataFrame)
+}
 
-# odavde vidimo da godine imaju dosta Nan-ova u treningu, pa cemo to zameniti medijanom
-sapply(train_data, function(x) sum(is.na(x)))
-
-# odavde vidimo da godine imaju dosta nanova u test-u, pa cemo to zameniti medijanom godina
-# Takodje i Fare ima 1 Nan. Posto je samo 1 u pitanju, odlucujemo se da ga izbacimo
-sapply(test_data, function(x) sum(is.na(x)))
-
-train_data <- age_median_replace(train_data)
-test_data <- age_median_replace(test_data)
-test_data <- na.omit(test_data)
-
-sapply(train_data, function(x) sum(is.na(x)))
-sapply(test_data, function(x) sum(is.na(x)))
+main()
